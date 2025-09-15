2518f1413791e7c651749711a9b024a8a0220074
Thanh_Dang <ntdang23@clc.fitus.edu.vn>
2025-09-15 20:21:01 +0700

    Delete dbConfig.ts

--- dbConfig.ts
+++
@@ -1,9 +0,0 @@
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions.js';
-
-export const pgConfig: PostgresConnectionOptions = {
-  url: 'postgresql://neondb_owner:npg_rO7tJkS0RIxg@ep-curly-bonus-a1vx2urs-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
-  type: 'postgres',
-  port: 3306,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
-};
