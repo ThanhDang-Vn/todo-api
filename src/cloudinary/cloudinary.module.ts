38ce1c5f0076ce6319ed69a667bd291fd9fd64b0
ThanhDang-Vn <ntdang23@clc.fitus.edu.vn>
2026-02-21 21:22:31 +0700

    refactor: :wrench: add provider

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CloudinaryService } from './cloudinary.service';
+import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
   imports: [PrismaModule],
-  providers: [CloudinaryService],
+  providers: [CloudinaryService, CloudinaryProvider],
   exports: [CloudinaryService],
 })
 export class CloudinaryModule {}
--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -2,11 +2,13 @@ import { v2 as cloudinary } from 'cloudinary';
 
 export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
-  userFactory: () => {
-    return cloudinary.config({
+  useFactory: () => {
+    cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
+
+    return cloudinary;
   },
 };
